b8c81798f9adf24de5b9f131e5dfb75ddeaae39e
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-10-28 03:27:11 +0900

    js

--- index.js
+++ index.js
@@ -95,16 +95,18 @@ function randomdraggable(obj) {
 }
 
 window.onload = () => {
-  const scrollElement = document.querySelector('#randomdraggable');
-  scrollElement.addEventListener('wheel', (e) => {
-    if (Math.abs(e.deltaY) < Math.abs(e.deltaX)) return;
-    const maxScrollLeft = scrollElement.scrollWidth - scrollElement.clientWidth;
-    if (
-      (scrollElement.scrollLeft <= 0 && e.deltaY < 0) ||
-      (scrollElement.scrollLeft >= maxScrollLeft && e.deltaY > 0)
-    )
-      return;
-    e.preventDefault();
-    scrollElement.scrollLeft += e.deltaY;
-  });
+  if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    const scrollElement = document.querySelector('#randomdraggable');
+    scrollElement.addEventListener('wheel', (e) => {
+      if (Math.abs(e.deltaY) < Math.abs(e.deltaX)) return;
+      const maxScrollLeft = scrollElement.scrollWidth - scrollElement.clientWidth;
+      if (
+        (scrollElement.scrollLeft <= 0 && e.deltaY < 0) ||
+        (scrollElement.scrollLeft >= maxScrollLeft && e.deltaY > 0)
+      )
+        return;
+      e.preventDefault();
+      scrollElement.scrollLeft += e.deltaY;
+    });
+  }
 };
