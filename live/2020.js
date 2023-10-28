32f15895044fa4b75ec740781811770fab62d936
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-10-29 03:17:28 +0900

    live

--- live/2020.js
+++ live/2020.js
@@ -1,16 +1,16 @@
 'use strict'
 
-const play = {
-  '3月8日': ['0308', 'AnqKtwk7mKU'],
-  '4月5日': ['0405', 'bVyJ3xpF9jI'],
-  '5月3日': ['0503', 'YrDo1Zg2avg'],
-  '6月7日': ['0607', 'LfpdOdqrqSI'],
-  '7月5日': ['0705', 'YcO45vs8_Tc'],
-  '8月2日': ['0802', 'MQsOJ9NNrho'],
-  '9月6日': ['0906', '4iRCoFB2qZw'],
-  '10月4日': ['1004', 'LPlODq-ZKTE'],
-  '11月1日': ['1101', '7UI5u-suxig'],
-  '12月6日': ['1206', 'Ur2Yxipmg5w'],
+const play2020 = {
+  '3月8日': ['0308', 'BnA Alter Museume にプレイタイムズを搬入をした記録'],
+  '4月5日': ['0405', '芥川亜由弥・中尾香織・ささじまかずま・船川翔司 が、花見をした記録'],
+  '5月3日': ['0503', 'woopheadclrms・2n2n・芥川亜由弥・中尾香織・ささじまかずま が、食事をした記録'],
+  '6月7日': ['0607', 'Ibuki Kakita・芥川亜由弥・中尾香織・ささじまかずま が、水を写した記録'],
+  '7月5日': ['0705', '芥川亜由弥・中尾香織・ささじまかずま が、風を映した記録'],
+  '8月2日': ['0802', '芥川亜由弥・中尾香織・ささじまかずま・船川翔司 が、昆虫を写した記録'],
+  '9月6日': ['0906', '宙空一派・Ibuki Kakita・芥川亜由弥・中尾香織・ささじまかずま が、近方ぼやぼやをした記録'],
+  '10月4日': ['1004', 'wanna be youth・2n2n・芥川亜由弥・中尾香織・ささじまかずま が、靴の上から靴を履いた記録'],
+  '11月1日': ['1101', 'Yuto Ohashi・Kenny Pain・芥川亜由弥・中尾香織・ささじまかずま が、空を見上げた記録'],
+  '12月6日': ['1206', 'Rie Kai・芥川亜由弥・中尾香織・ささじまかずま が、何かに落ちたり何かを落とした記録'],
 }
 
 document.addEventListener('readystatechange', event => {
@@ -22,7 +22,7 @@ document.addEventListener('readystatechange', event => {
     option.setAttribute("selected", "true");
     select.appendChild(option);
 
-    const live = Object.entries(play);
+    const live = Object.entries(play2020);
     live.forEach((src) => {
       const option = document.createElement('option');
       option.innerText = src[0] + 'のプレイタイムズ';
