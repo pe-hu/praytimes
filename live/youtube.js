eed5e43a073978bab8502dd412c82d47df3e819e
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-10-29 01:07:28 +0900

    live

--- live/youtube.js
+++ live/youtube.js
@@ -1,11 +1,11 @@
 'use strict'
 
 async function indexJSON(requestURL) {
-    const request = new Request(requestURL);
-    const response = await fetch(request);
-    const jsonIndex = await response.text();
-    const index = JSON.parse(jsonIndex);
-    youtube(index);
+  const request = new Request(requestURL);
+  const response = await fetch(request);
+  const jsonIndex = await response.text();
+  const index = JSON.parse(jsonIndex);
+  youtube(index);
 }
 
 function shuffle(arrays) {
@@ -18,17 +18,28 @@ function shuffle(arrays) {
 }
 
 function youtube(obj) {
-    const randomdraggable = document.querySelector('#randomdraggable');
-    const youtube = shuffle(obj.youtube)
-    for (let i = 0; i < youtube.length; i++) {
-        const li = document.createElement('li');
-        const iframe = document.createElement('iframe');
-        iframe.setAttribute("src", 'https://www.youtube.com/embed/' + youtube[i].id + '?controls=0&disablekb=1&playsinline=1&modestbranding=1&rel=0');
-        iframe.setAttribute("allowfullscreen", "");
-        iframe.setAttribute("playsinline", "");
-        iframe.setAttribute("frameborder", "0");
+  const player = document.querySelector('#player');
+  const randomdraggable = document.querySelector('#randomdraggable');
+  const youtube = shuffle(obj.youtube)
+  for (let i = 0; i < youtube.length; i++) {
+    if (i === 0) {
+      const iframe = document.createElement('iframe');
+      iframe.setAttribute("src", 'https://www.youtube.com/embed/' + youtube[i].id + '?controls=0&disablekb=1&playsinline=1&modestbranding=1&rel=0');
+      iframe.setAttribute("allowfullscreen", "");
+      iframe.setAttribute("playsinline", "");
+      iframe.setAttribute("frameborder", "0");
+      iframe.id = "one"
+      player.appendChild(iframe);
+    } else {
+      const li = document.createElement('li');
+      const iframe = document.createElement('iframe');
+      iframe.setAttribute("src", 'https://www.youtube.com/embed/' + youtube[i].id + '?controls=0&disablekb=1&playsinline=1&modestbranding=1&rel=0');
+      iframe.setAttribute("allowfullscreen", "");
+      iframe.setAttribute("playsinline", "");
+      iframe.setAttribute("frameborder", "0");
 
-        li.appendChild(iframe);
-        randomdraggable.appendChild(li);
+      li.appendChild(iframe);
+      randomdraggable.appendChild(li);
     }
+  }
 }
