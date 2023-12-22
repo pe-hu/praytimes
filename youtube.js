9f0dd1366bc3a22e3009ec329fbf515f6d3c2250
∧° ┐ <we.are.pe.hu@gmail.com>
2023-12-22 17:41:54 +0900

    youtube

--- youtube.js
+++ youtube.js
@@ -1,15 +1,15 @@
 'use strict'
 
+const tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 var ii = 0;
 var allID;
 
 var player;
 var videoId;
-
-const tag = document.createElement('script');
-tag.src = "https://www.youtube.com/player_api";
-const firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 function onYouTubePlayerAPIReady() {
     player = new YT.Player('player', {
@@ -19,14 +19,22 @@ function onYouTubePlayerAPIReady() {
             'controls': 0,
             'rel': 0
         }
-    });
+    })
+}
+
+function shuffle(arrays) {
+    const array = arrays.slice();
+    for (let i = array.length - 1; i >= 0; i--) {
+        const shuffleArr = Math.floor(Math.random() * (i + 1));
+        [array[i], array[shuffleArr]] = [array[shuffleArr], array[i]];
+    }
+    return array;
 }
 
 function onGoogleLoad() {
+    const result = document.getElementById('randomdraggable');
     gapi.client.setApiKey('AIzaSyDwAKBrqYSCArG1B_Y99BswIvz3IfumosE');
     gapi.client.load('youtube', 'v3', function () {
-        const result = document.getElementById('randomdraggable');
-
         var request = gapi.client.youtube.playlistItems.list({
             playlistId: 'PLNxErRQ8jPVZl46rUXgcLHhaR5AbDW6Cn',
             part: 'snippet,contentDetails',
@@ -48,39 +56,22 @@ function onGoogleLoad() {
                 input.setAttribute('name', 'youtube');
                 input.id = thisID;
                 input.value = thisID;
-                input.dataset.title = thisTitle;
-                input.dataset.no = i;
                 li.appendChild(input);
 
                 const label = document.createElement('label');
                 label.setAttribute('for', thisID);
-                label.innerText = thisTitle;
                 li.appendChild(label);
 
-                if (i === 0) {
-                    input.checked = true;
-                    ii = input.dataset.no;
-                    videoId = input.value;
-                } else if (i === items.length - 1) {
-                    allID = input.dataset.no;
-                }
+                const img = document.createElement('img');
+                img.src = 'https://img.youtube.com/vi/' + thisID + '/default.jpg';
+                label.appendChild(img);
 
                 input.addEventListener('click', () => {
                     videoId = input.value;
-                    ii = input.dataset.no;
                     player.loadVideoById({ videoId: videoId });
                 });
             }
             player.loadVideoById({ videoId: videoId });
         });
     });
-}
-
-function shuffle(arrays) {
-    const array = arrays.slice();
-    for (let i = array.length - 1; i >= 0; i--) {
-        const shuffleArr = Math.floor(Math.random() * (i + 1));
-        [array[i], array[shuffleArr]] = [array[shuffleArr], array[i]];
-    }
-    return array;
 }
